36bbb364d7493986d44923d315647b05a84c6cb2
Akshay Patil <53465888+AkshayPatil4@users.noreply.github.com>
2023-10-31 23:11:30 +0100

    login completed

--- src/app/auth/signup/auth.service.ts
+++ src/app/auth/signup/auth.service.ts
@@ -1,12 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable} from  '@angular/core'
+import { catchError, throwError } from 'rxjs';
 
-interface AuthResponseData{
+export interface AuthResponseData{
     idToken :string,
     email: string,
     refreshToken:string,
     expiresIn: string,
-    localId: string
+    localId: string,
+    registered?:boolean
 }
 @Injectable({providedIn: 'root'})
 
@@ -20,6 +22,31 @@ onSignup(email: string, password:string){
         email:email,
         password: password,
         returnSecureToken:true
-    } );
+    } ).pipe(catchError(errorRes=>{
+        let errorMessage = 'An unknown error occured';
+        if(!errorRes.error || !errorRes.error.error){
+            return throwError(errorMessage);
+        }
+        switch(errorRes.error.error.message){
+            case 'EMAIL_EXISTS' : 
+            errorMessage = 'The email address is already in use by another account';
+            break;
+            case 'EMAIL_NOT_FOUND' :
+                errorMessage = 'There is no user record corresponding to this identifier';
+                break;
+            case 'INVALID_PASSWORD' :
+                errorMessage = 'The password is invalid ';
+                break;
+        }
+       return throwError(errorMessage);
+    }));
+}
+
+login(email: string, password:string){
+    return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAecRGdf8AvP-I0T8c6gv0qpFX6cBJYYfc',{
+    email: email,
+    password: password,
+    returnSecureToken:true
+    })
 }
 }
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -6,7 +6,7 @@ import {
   FormGroup,
   Validators,
 } from '@angular/forms';
-import { Subject, tap } from 'rxjs';
+import { Observable, Subject, tap } from 'rxjs';
 import { User } from '../user-model';
 import { AuthService } from './auth.service';
 
@@ -24,13 +24,13 @@ interface authresponseData {
   styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
-  constructor(private http: HttpClient, private authService : AuthService) {}
+  constructor(private http: HttpClient, private authService: AuthService) {}
   isLoginMode = true;
   signupForm!: FormGroup;
- 
+
   error: string = '';
- 
-  onSwitchMode(){
+
+  onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
   }
   ngOnInit(): void {
@@ -42,24 +42,25 @@ export class SignupComponent implements OnInit {
   onSubmit() {
     if (this.signupForm.invalid) {
       return;
-    } 
-      const formData = this.signupForm.value;
+    }
+    const formData = this.signupForm.value;
+    let authObs: Observable<authresponseData>;
 
-      if(this.isLoginMode){
+    if (this.isLoginMode) {
+      authObs = this.authService.login(formData.email, formData.password);
+    } else {
+      authObs = this.authService.onSignup(formData.email, formData.password);
+    }
 
+    authObs.subscribe(
+      (resData) => {
+        console.log(resData);
+      },
+      (errorMessage) => {
+        this.error = errorMessage;
       }
+    );
 
-      else{
-        this.authService.onSignup(formData.email, formData.password).subscribe(resData=>{
-          console.log(resData);
-        },
-        erorrRes=>{
-          console.log(erorrRes);
-        });
-      }
-      
-      
-      
     this.signupForm.reset();
   }
 }
